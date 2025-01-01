3a7959302217f795992a263f058ae5a1bb578ef6
kojamad28 <kojamad28@gmail.com>
2025-01-01 18:39:19 +0900

    merged env files

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,8 @@
 from dotenv import dotenv_values
 from sqlalchemy import URL
 
-app_config = dotenv_values('instance/.env.dev')
-#app_config = dotenv_values('instance/.env')
+#app_config = dotenv_values('instance/.env.dev')
+app_config = dotenv_values('instance/.env')
 
 DEBUG = app_config.get('DEBUG', 'false').lower() == 'true'
 
@@ -10,12 +10,12 @@ SECRET_KEY = app_config['SECRET_KEY']
 
 DATABASE_URL = URL.create(
         'postgresql',
-        username=app_config['DATABASE_USER'],
-        password=app_config['DATABASE_PASSWORD'],  # plain (unescaped) text
-        host=app_config.get('DATABASE_HOST', 'postgres'),
-        port=app_config.get('DATABASE_PORT', '5432'),
-        database=app_config.get('DATABASE_DB', 'postgres'),
-        query={'options': f'-c search_path={app_config.get("DATABASE_SCHEMA", "public")}'},
+        username=app_config['POSTGRES_USER'],
+        password=app_config['POSTGRES_PASSWORD'],  # plain (unescaped) text
+        host=app_config.get('POSTGRES_HOST', 'postgres'),
+        port=app_config.get('POSTGRES_PORT', '5432'),
+        database=app_config.get('POSTGRES_DB', 'postgres'),
+        query={'options': f'-c search_path={app_config.get("POSTGRES_SCHEMA", "public")}'},
 )
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
